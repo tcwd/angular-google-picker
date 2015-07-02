0821c871e128e2c2cf86ec4eee1c57df09433241
Jason Kurian <jasonk92@gmail.com>
2015-07-01 20:43:10 -0400

    Add test task dependency for copy and uglify
    - updated npm and bower devDependencies
    - added spec reporter for tests

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,17 @@
 var gulp = require('gulp');
-var uglify = require('gulp-uglify');
+
+var karma = require('karma').server;
 var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
 
 // Copy non-uglify version to "dist"
-gulp.task('copy', function () {
+gulp.task('copy', ['test'], function () {
   return gulp.src('src/*.js')
     .pipe(gulp.dest('dist'));
 });
 
 // Create an uglify version to "dist" and "example"
-gulp.task('uglify', function () {
+gulp.task('uglify', ['test'], function () {
   return gulp.src('src/*.js')
     .pipe(uglify())
     .pipe(rename('google-picker.min.js'))
@@ -18,7 +20,14 @@ gulp.task('uglify', function () {
 });
 
 gulp.task('watch', function () {
-  gulp.watch('src/*.js', ['copy', 'uglify'])
+  gulp.watch('src/*.js', ['copy', 'uglify']);
 });
 
-gulp.task('default', ['copy', 'uglify']);
+gulp.task('test', function (done) {
+  karma.start({
+    configFile: __dirname + '/test/karma.conf.js',
+    singleRun: true
+  }, done);
+});
+
+gulp.task('default', ['test', 'copy', 'uglify']);
